60c409d7b9bfe4722dbff59df0319c92c8dd7cbf
Dylan Todd <dylan.todd04@gmail.com>
2025-01-18 21:55:57 -0500

    feat: Color filter component

---
+++ src/lib/components/colorfilter.svelte
@@ -0,0 +1,44 @@
+<!-- src/lib/components/ColorFilter.svelte -->
+<script lang="ts">
+    export let color: string = '#00ff00';
+    export let opacity: number = 0.3;
+</script>
+
+<div class="color-filter">
+    <slot></slot>
+    <div class="overlay-color" style="--filter-color: {color}; --filter-opacity: {opacity}"></div>
+    <div class="overlay-desaturate"></div>
+</div>
+
+<style>
+    .color-filter {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+
+    .overlay-color {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: var(--filter-color);
+        opacity: var(--filter-opacity);
+        z-index: 4;
+        mix-blend-mode: color;
+        pointer-events: none;
+    }
+
+    .overlay-desaturate {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: #808080;
+        z-index: 3;
+        mix-blend-mode: saturation;
+        pointer-events: none;
+    }
+</style>
