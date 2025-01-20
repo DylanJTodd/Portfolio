dbb4cc9cb671826273b8132649eb3aee379697a3
Dylan Todd <dylan.todd04@gmail.com>
2025-01-19 21:20:37 -0500

    Added event handling and making choices dissapear

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
     import TextScroll from '$lib/components/textscroll.svelte';
+    let choiceList: HTMLParagraphElement;
 </script>
 
 <section class="terminal-opening">
@@ -7,11 +8,15 @@
     <TextScroll startDelay={1000} audioPlay={false} typingSpeed={75} text="Setting up configuration..."/>
     <TextScroll startDelay={400} audioPlay={false} typingSpeed={75} text="..."/>
     <TextScroll startDelay={700} audioPlay={false} typingSpeed={75} text="..."/><br>
-    <TextScroll startDelay={1000} audioPlay={false} typingSpeed={75} text="Enable Audio?"/>
+    <TextScroll startDelay={1000} audioPlay={false} typingSpeed={75} text="Enable Audio?" on:animationComplete={() => { 
+        if (choiceList) {
+            choiceList.style.visibility = 'visible';
+        }
+    }}/>
     <TextScroll hideCaretManually={true} startDelay={500} audioPlay={false} /><br>
 
 
-    <p class="choice-list">
+    <p class="choice-list" bind:this={choiceList}>
         <button class="terminal-choice choice-selected">[Enable Audio]</button><br>
         <button class="terminal-choice">[Disable Audio]</button>
     </p>
