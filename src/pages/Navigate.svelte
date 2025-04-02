1eb47f7102b0adffc598448e6defb7ac2629cc8b
Dylan Todd <dylan.todd04@gmail.com>
2025-04-01 22:12:36 -0400

    chore: Add disclaimer for server time

--- src/pages/Navigate.svelte
+++ src/pages/Navigate.svelte
@@ -27,7 +27,7 @@
 <section class="terminal-opening" bind:this={terminalSection} in:fly="{{ y: 0, duration: 1000 }}" out:fly="{{ y: -1000, duration: 1000 }}">
     <TextScroll audioPlay={$audioEnabled} typingSpeed={50} text="Welcome to my portfolio website!"/>
     {#if $lastLogin}
-        <TextScroll startDelay={2000} audioPlay={$audioEnabled} typingSpeed={50} text="Your last login was on {$lastLogin}."/>
+        <TextScroll startDelay={2000} audioPlay={$audioEnabled} typingSpeed={50} text="Your last login was on {$lastLogin} (Server time)."/>
     {/if}
     <TextScroll startDelay={1000} audioPlay={$audioEnabled} typingSpeed={50} text="This is a main directory listing. Feel free to navigate to any of the below pages." on:animationComplete={() => { 
         if (choiceList) {
